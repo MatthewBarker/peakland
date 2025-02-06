299cc45c5b73488346d6f5a295c0fd4aee303ad0
Matt Barker <mrjbarker@hotmail.com>
2025-02-06 16:34:21 +0000

    feat: add seventh series

---
+++ maps/historic-seventh.js
@@ -0,0 +1,35 @@
+const key = await fetch("../keys/map-tiler.txt");
+
+maptilersdk.config.apiKey = await key.text();
+
+const map = new maptilersdk.Map({
+    container: "map",
+    style: {
+        version: 8,
+        sources: {
+            historic: {
+                type: "raster",
+                tiles: [
+                    `https://api.maptiler.com/tiles/uk-osgb63k1955/{z}/{x}/{y}.jpg`,
+                ], // first one-inch to the mile to cover the whole of Great Britain
+            },
+        },
+        layers: [
+            {
+                id: "historic",
+                type: "raster",
+                source: "historic",
+                minzoom: 1,
+                maxzoom: 15,
+            },
+        ],
+    },
+});
+
+const onLoad = async () => {
+    map.fitBounds([-2.107998, 53.033855, -1.515579, 53.597859], {
+        padding: 20,
+    });
+};
+
+map.on("load", onLoad);
--- maps/leisure.js
+++ maps/leisure.js
@@ -2,6 +2,9 @@ const keyResponse = await fetch("../keys/os.txt");
 const key = await keyResponse.text();
 const api = "https://api.os.uk/maps/raster/v1/zxy/Leisure_27700/{z}/{x}/{y}.png?key=" + key;
 
+const mapMarkerResponse = await fetch("../assets/map-marker-1.svg");
+const mapMarker = await mapMarkerResponse.text();
+
 // setup the EPSG:27700 (British National Grid) projection
 const crs = new L.Proj.CRS(
     "EPSG:27700",
@@ -17,3 +20,43 @@ const bounds = new L.LatLngBounds([[53.033855, -2.107998], [53.597859, -1.515579
 
 L.tileLayer(api).addTo(map);
 map.fitBounds(bounds);
+
+function onLocationFound(e) {
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + e.accuracy + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+L.Control.Locate = L.Control.extend({
+    onAdd: function(map) {
+        const container = L.DomUtil.create("div", "leaflet-bar leaflet-control");
+        const button = L.DomUtil.create("a", "leaflet-control-button", container);
+
+        L.DomEvent.disableClickPropagation(button);
+        
+        L.DomEvent.on(button, "click", function(){
+            map.locate({setView: true, maxZoom: 16});
+        });
+
+        container.title = "Locate";
+        button.style.cursor = "pointer";
+        button.innerHTML = mapMarker;
+
+        return container;
+    },
+
+    onRemove: function(map) {}
+});
+
+L.control.locate = function(options) {
+    return new L.Control.Locate(options);
+}
+
+map.on("locationfound", onLocationFound);
+map.on("locationerror", onLocationError);
+L.control.locate({ position: "topleft" }).addTo(map);
